69b6d0b75dcc193077a52ba3ea8ba0d3fcf53d26
ZuForSure <anhvux20se@gmail.com>
2025-07-28 17:17:07 +0700

    Lesson 3 - Mini Game: Tank Shooter

---
+++ Assets/Lesson3/Scripts/FastInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastInfo : MonoBehaviour
+{
+    // Phần này sẽ là sử dụng Pooling Object trong tình huống thực tế
+    // Sử dụng Package LeanPool
+    // Đưa ra ưu điểm và nhược điểm của LeanPool
+    // Sẽ có thêm 1 ví dụ do anh Bảo làm
+
+    // Một game xe tăng top town, di chuyển bắn kẻ địch để sống sót càng lâu càng tốt
+    // WASD di chuyển
+    // chuột trái: bắn
+}
