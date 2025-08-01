bf87de08bcd8ca30336ff8d534838a5aa748dd88
ZuForSure <anhvux20se@gmail.com>
2025-08-01 18:11:46 +0700

    Mirror test

---
+++ Assets/TestMirror/CustomNetworkManager.cs
@@ -0,0 +1,59 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomNetworkManager : NetworkManager
+{
+    [SerializeField] GameObject prefab;
+
+    public override void OnStartHost()
+    {
+        base.OnStartHost();
+        Debug.Log("OnStartHost");
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        Debug.Log("OnStartClient");
+    }
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        Debug.Log("OnStartServer");
+        //GameObject obj = Instantiate(prefab);
+        //NetworkServer.Spawn(obj);
+    }
+
+    public override void OnClientConnect()
+    {
+        base.OnClientConnect();
+        Debug.Log("OnClientConnect");
+    }
+
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        base.OnServerAddPlayer(conn);
+        Debug.Log("OnServerAddPlayer");
+    }
+
+    public override void OnClientDisconnect()
+    {
+        base.OnClientDisconnect();
+        Debug.Log("OnClientDisconnect");
+    }
+
+    public override void OnServerConnect(NetworkConnectionToClient conn)
+    {
+        base.OnServerConnect(conn);
+        Debug.Log("OnServerConnect");
+    }
+
+    public override void OnServerDisconnect(NetworkConnectionToClient conn)
+    {
+        base.OnServerDisconnect(conn);
+        Debug.Log("OnServerDisconnect");
+    }
+}
