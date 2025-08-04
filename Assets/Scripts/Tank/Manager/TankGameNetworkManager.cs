39c0bdeb52a9704c447e05600d1191f3bf388513
Gia Bao <capricos2212@gmail.com>
2025-08-04 14:27:41 +0700

    [update] scene tank

--- Assets/Scripts/Tank/Manager/TankGameNetworkManager.cs
+++ Assets/Scripts/Tank/Manager/TankGameNetworkManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TankNetworkManager : NetworkManager
+public class TankGameNetworkManager : NetworkManager
 {
     public override void OnServerAddPlayer(NetworkConnectionToClient conn)
     {
